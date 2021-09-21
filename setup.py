e328b1862f1fae3698f05c242b20fc4a1f2a5a5f
Bradley Bell <bradley.bell@gmail.com>
2021-09-21 00:57:54 -0700

    allow newer django

--- setup.py
+++ setup.py
@@ -22,7 +22,8 @@ setup(
     include_package_data=True,
     install_requires = [
         'AuthZ-Group',
-        'Django>=1.11.19,<2.0',
+        'Django>=1.11.19,<2.0 ; python_version < "3.0"',
+        'Django>=1.11.19 ; python_version >= "3.0"',
         'django-compressor',
         'Django-SupportTools<3.0 ; python_version < "3.0"',
         'Django-SupportTools ; python_version >= "3.0"',
